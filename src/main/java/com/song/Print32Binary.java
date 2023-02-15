1d3e15592604d005b65a60a6b0db502216c3f3a9
‘explore11’ <prefect_start@163.com>
2023-02-15 15:00:51 +0800

    Initial commit

---
+++ src/main/java/com/song/Print32Binary.java
@@ -0,0 +1,23 @@
+package com.song;
+
+/**
+ * 打印一个整数Int的32位二进制数
+ */
+public class Print32Binary {
+    /**
+     * 打印二进制数据
+     *
+     * @param num
+     */
+    public static void print(int num) {
+        for (int i = 31; i >= 0; i--) {
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int num = 1;
+        print(num);
+    }
+}
